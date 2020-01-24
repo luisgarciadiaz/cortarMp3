87045f90f50ccccccdabb2614f697d4a35ccac04
luisgarciadiaz <zethes@gmail.com>
2020-01-24 17:54:44 -0600

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,38 +1,142 @@
 import os
 import subprocess
+import random
+import re
 from time import time
 clear=lambda:os.system('cls')
-clear()
-os.chdir('C://Users/luis/Desktop/prueba/')
-path='C://Users/luis/Desktop/prueba/'
+clear() #borra la pantalla
+os.chdir('C:/Users/luis/Documents/GitHub/cortarMp3 ')
+path='C:/Users/luis/Documents/GitHub/cortarMp3' #path
 files =[]
 tiempo_inicial=time()
 print("empezando a leer archivos")
-i=0
+i=0#contador de archivos
 for r, d, f in os.walk(path):
     for file in f:
         if '.mp3' in file:
-#files.append(os.path.join(r, file))
-            args=("ffprobe","-show_entries", "format=duration","-i",file,'-hide_banner','-loglevel','panic',)
-            popen=subprocess.Popen(args,stdout =subprocess.PIPE)
-            popen.wait()
-            output=popen.stdout.read()
+            output=0
             files.append([file,output])
             i=i+1
+ #termina lectura de la carpeta      
 numeroArchivos=i
 tiempo_final=time()
 tiempo_ejecución=tiempo_final-tiempo_inicial
 print("terminado de leer archivos tardo:"+str(tiempo_ejecución)+" en segundos un total de "+ str(i))
 tiempo_inicial=time()
 i=0 
-print("empezando a cortar")        
+#cuanto tiempo tardo en realizar el proceso 
+print("empezando a cortar inicial") 
+inicial=30
+tiempo=30   
 for f in files:
-#print(f)
-    subprocess.call(['ffmpeg','-y', '-ss','30','-i',f[0],'-t','120','-c','copy',str(i)+f[0],'-hide_banner','-loglevel','panic',])
-    i+1
+    i=i+1
+    
+    #subprocess.call(['ffmpeg','-y', '-ss',str(inicial),'-i',f[0],'-sseof',str(final),'-t',str(tiempo),'-c','copy',str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+    subprocess.call(['ffmpeg','-y', '-ss',str(inicial),'-i',f[0],'-c','copy',"./transformados/"+str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+#-ss specifies the start time, e.g. 00:01:23.000 or 83 (in seconds)
+#-t specifies the duration of the clip (same format).
+#Recent ffmpeg also has a flag to supply the end time with -to.
+#-c copy copies the first video, audio, and subtitle bitstream from the input to the output file without re-encoding them. This won't harm the quality and make the command run within seconds.
 tiempo_final=time()
 tiempo_ejecución=tiempo_final-tiempo_inicial
-print("terminado de leer archivos tardo:")
-print(tiempo_ejecución)
-print (" en segundos")
-print("listo")
+print("terminado de leer archivos tardo:"+str(tiempo_ejecución)+ " en segundos")
+#termino de quitar los 30 segundos del inicio
+
+
+
+os.chdir('C:/Users/luis/Documents/GitHub/cortarMp3/transformados ')
+path='C:/Users/luis/Documents/GitHub/cortarMp3/transformados' #path
+files =[]
+tiempo_inicial=time()
+print("empezando a leer archivos transformados")
+i=0#contador de archivos
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.mp3' in file:
+            args=("ffprobe","-show_entries", "format=duration","-i",file,'-hide_banner','-loglevel','panic',)
+            popen=subprocess.Popen(args,stdout =subprocess.PIPE)
+            popen.wait()
+            output=popen.stdout.read()
+            files.append([file,output])
+            i=i+1
+ #termina lectura de la carpeta      
+numeroArchivos=i
+tiempo_final=time()
+tiempo_ejecución2=tiempo_final-tiempo_inicial
+print("terminado de leer archivos tardo:"+str(tiempo_ejecución2)+" en segundos un total de "+ str(i))
+tiempo_inicial=time()
+i=0 
+#cuanto tiempo tardo en realizar el proceso 
+print("empezando a cortar de los 30 final") 
+final=-30
+tiempo=30   
+for f in files:
+#print(f)
+    i=i+1
+    tiempoCadena=f[1].decode("utf-8")
+    duracion=tiempoCadena.split("=")
+    duracion=duracion[1]
+    duracion2=duracion.split('\r')
+    duracion=duracion2[0]
+    duracion=re.sub('[^A-Za-z0-9]+', '', duracion)
+    duracion=int(duracion)/1000000
+    duracion_real=duracion-30 
+    #subprocess.call(['ffmpeg','-y', '-ss',str(inicial),'-i',f[0],'-sseof',str(final),'-t',str(tiempo),'-c','copy',str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+    subprocess.call(['ffmpeg','-y','-t',str(duracion_real) ,'-i',f[0],'-c','copy',"../transformados2/"+str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+tiempo_final=time()
+tiempo_ejecución=tiempo_final-tiempo_inicial
+print("terminado de leer archivos tardo:"+str(tiempo_ejecución)+" en segundos")
+
+
+
+#ahora al azar 
+os.chdir('C:/Users/luis/Documents/GitHub/cortarMp3/transformados2 ')
+path='C:/Users/luis/Documents/GitHub/cortarMp3/transformados2' #path
+files =[]
+tiempo_inicial=time()
+print("empezando a leer archivos transformados2")
+i=0#contador de archivos
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.mp3' in file:
+            args=("ffprobe","-show_entries", "format=duration","-i",file,'-hide_banner','-loglevel','panic',)
+            popen=subprocess.Popen(args,stdout =subprocess.PIPE)
+            popen.wait()
+            output=popen.stdout.read()
+            files.append([file,output])
+            i=i+1
+ #termina lectura de la carpeta      
+numeroArchivos=i
+tiempo_final=time()
+tiempo_ejecución3=tiempo_final-tiempo_inicial
+print("terminado de leer archivos tardo:"+str(tiempo_ejecución3)+" en segundos un total de "+ str(i))
+tiempo_inicial=time()
+i=0 
+#cuanto tiempo tardo en realizar el proceso 
+print("empezando a cortar final") 
+inicial=30
+tiempo=30   
+for f in files:
+#print(f)
+    i=i+1
+    tiempoCadena=f[1].decode("utf-8")
+    duracion=tiempoCadena.split("=")
+    duracion=duracion[1]
+    duracion2=duracion.split('\r')
+    duracion=duracion2[0]
+    duracion=re.sub('[^A-Za-z0-9]+', '', duracion)
+    duracion=int(duracion)/1000000
+    duracion_real=duracion-30 
+    print(duracion_real)
+    inicial=random.randint(1,int(duracion_real))
+    #subprocess.call(['ffmpeg','-y', '-ss',str(inicial),'-i',f[0],'-sseof',str(final),'-t',str(tiempo),'-c','copy',str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+    subprocess.call(['ffmpeg','-y', '-ss',str(inicial),'-i',f[0],'-c','copy','-t', '30',"../final/"+str(i)+"-"+f[0],'-hide_banner','-loglevel','panic',])
+#-ss specifies the start time, e.g. 00:01:23.000 or 83 (in seconds)
+#-t specifies the duration of the clip (same format).
+#Recent ffmpeg also has a flag to supply the end time with -to.
+#-c copy copies the first video, audio, and subtitle bitstream from the input to the output file without re-encoding them. This won't harm the quality and make the command run within seconds.
+tiempo_final=time()
+tiempo_ejecución=tiempo_final-tiempo_inicial
+print("terminado de leer archivos tardo:"+str(tiempo_ejecución)+" en segundos")
+tiempo_total= tiempo_ejecución+tiempo_ejecución2+tiempo_ejecución3
+print("listo en "+str(tiempo_total))
